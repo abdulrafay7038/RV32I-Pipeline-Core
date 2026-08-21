594d03dce0e0bcebb71e051d43a59a41184a547f
Abdul Rafay <abdulrafay7038@gmail.com>
2026-08-21 16:12:02 +0500

    Branch Target Buffer RTL

--- rtl/branchpredictor/BTB.sv
+++ rtl/branchpredictor/BTB.sv
@@ -1,33 +1,53 @@
 module BTB(
-    input  var logic        clk,
-    input  var logic        rst,
-    input  var logic        read,
-    input  var logic [31:0] pc1,
-    input  var logic [31:0] pc2,
-    input  var logic [31:0] target_address,
-    input  var logic        actual,
-    input  var logic [1:0]  prediction,
-    output var logic [31:0] pc_in
+    input  logic        CLK,
+    input  logic        RST,
+    input  logic        WE,   // write-enable
+
+    input  logic [31:0] PCF,  // to read BTB during fetch
+    input  logic [31:0] PCE,  // to write BTB during execute
+    input  logic [31:0] actual_address,  //Branch target calculated during execute
+
+    output logic [31:0] target_address,  //target address saved in BTB
+    output logic        hit                 
+
 );
+    logic [54:0] btb [255:0];   //256 entries in table each 55 bit wide, 22 tag bits + 32 bits for target address, 1 bit for valid
+    logic [7:0]  indexF;
+    logic [7:0]  indexE;
+    logic [21:0] tagF;
+    logic [21:0] tagE;
+    logic [21:0] stored_tag;
+    logic valid;
 
-    var logic [53:0] btb [255:0];
-    var logic [21:0] tag1;
-    var logic [ 7:0] index1;
-    var logic [21:0] tag2;
-    var logic [ 7:0] index2;
+    //Decode
+    assign tagF   = PCF[31:10];
+    assign tagE   = PCE[31:10];
+    assign indexE = PCE[9:2];
+    assign indexF = PCF[9:2];
 
-//ASSIGN TAG AND INDEX
+    assign stored_tag = btb[indexF][53:32];
+    assign valid      = btb[indexF][54];
+
+    //Write Interface
+    always_ff @(posedge CLK or posedge RST) begin
+        if (RST) begin
+            for (int i = 0; i < 256: i++) begin
+                btb[i][54] <= 1'b0;                   //Initializing all valids to 0
+            end
+        end
+        else if (WE) begin
+            btb[indexE][31:0]  <= actual_address;
+            btb[indexE][53:32] <= tagE;
+            btb[indexE][54]    <= 1'b1;
+        end
+
+    end
+    
+    //Read Interface
     always_comb begin
-        // TODO
+        target_address = btb[indexF][31:0];
+        hit            = (tagF == stored_tag) && valid;
     end
 
-//GET TARGET ADDRESS
-    always_comb  begin
-        // TODO
-    end
 
-//WRITE OR REMOVE
-    always_ff @(negedge clk) begin
-        // TODO
-    end
 endmodule
