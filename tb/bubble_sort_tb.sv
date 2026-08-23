b00969b1ba2270e0d9db2338a8031b58679febbc
Abdul Rafay <abdulrafay7038@gmail.com>
2026-08-24 02:46:18 +0500

    Updated test bench files

--- tb/bubble_sort_tb.sv
+++ tb/bubble_sort_tb.sv
@@ -21,8 +21,44 @@ module bubblesort_tb;
         // Hold reset for one clock cycles
         @(posedge clk);
         rst = 0;
-        @(negedge clk) DUT.Datapath.Regfile.x[2] = 6140; // Initialize Stack Pointer
+        @(negedge clk) DUT.Datapath.Regfile.x[2] = 4095; // Initialize Stack Pointer
+        #5000000;
 
+        for (int i = 0; i < 255; i++) begin
 
+            logic [31:0] word_i;
+            logic [31:0] word_next;
+
+            word_i = {
+                DUT.Datapath.DataMem.data_mem[i*4+3],
+                DUT.Datapath.DataMem.data_mem[i*4+2],
+                DUT.Datapath.DataMem.data_mem[i*4+1],
+                DUT.Datapath.DataMem.data_mem[i*4+0]
+            };
+
+            word_next = {
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+3],
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+2],
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+1],
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+0]
+            };
+
+            if (word_i > word_next) begin
+                $fatal(
+                    1,
+                    "BUBBLESORT FAILED: word[%0d] = %0d > word[%0d] = %0d",
+                    i,
+                    word_i,
+                    i+1,
+                    word_next
+                );
+        end
     end
+            $display("----------------------------------------");
+            $display("BUBBLESORT PASSED");
+            $display("First 256 words are sorted.");
+            $display("----------------------------------------");
+
+            $finish;
+end
 endmodule        
--- tb/merge_sort_tb.sv
+++ tb/merge_sort_tb.sv
@@ -21,8 +21,45 @@ module mergesort_tb;
         // Hold reset for one clock cycles
         @(posedge clk);
         rst = 0;
-        @(negedge clk) DUT.Datapath.Regfile.x[2] = 6140; // Initialize Stack Pointer
+        @(negedge clk) DUT.Datapath.Regfile.x[2] = 4092; // Initialize Stack Pointer
 
+        #1000000;
 
+        for (int i = 0; i < 255; i++) begin
+
+            logic [31:0] word_i;
+            logic [31:0] word_next;
+
+            word_i = {
+                DUT.Datapath.DataMem.data_mem[i*4+3],
+                DUT.Datapath.DataMem.data_mem[i*4+2],
+                DUT.Datapath.DataMem.data_mem[i*4+1],
+                DUT.Datapath.DataMem.data_mem[i*4+0]
+            };
+
+            word_next = {
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+3],
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+2],
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+1],
+                DUT.Datapath.DataMem.data_mem[(i+1)*4+0]
+            };
+
+            if (word_i > word_next) begin
+                $fatal(
+                    1,
+                    "MERGESORT FAILED: word[%0d] = %0d > word[%0d] = %0d",
+                    i,
+                    word_i,
+                    i+1,
+                    word_next
+                );
+        end
+    end
+            $display("----------------------------------------");
+            $display("MERGESORT PASSED");
+            $display("First 256 words are sorted.");
+            $display("----------------------------------------");
+
+            $finish;
     end
 endmodule        
